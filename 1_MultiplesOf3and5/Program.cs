cbfb8758980c96b0fee63718be3a87ec5843475a
kry <kry.ban@gmail.nl>
2019-11-27 20:09:56 +0100

    Add project files.

---
+++ 1_MultiplesOf3and5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_MultiplesOf3and5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
