c71c61d19314190df5a7b70d2f3dc9bd97466b3b
kry <kry.ban@gmail.nl>
2019-11-27 20:31:52 +0100

    solution added

--- 1_MultiplesOf3and5/Program.cs
+++ 1_MultiplesOf3and5/Program.cs
@@ -2,11 +2,31 @@
 
 namespace _1_MultiplesOf3and5
 {
+    /*
+     * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+     * The sum of these multiples is 23.
+     * Find the sum of all the multiples of 3 or 5 below 1000.
+     */
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int solution = 0;
+
+            for(int i = 0; i<1000; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    solution += i;
+                    continue;
+                }
+            }
+            
+            Console.WriteLine($"Solution Project 1: {solution}");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key.");
+            Console.ReadKey();
         }
     }
 }
